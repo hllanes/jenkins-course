4ac13ea2c35c6d94826d375d9aad64c843579c9b
Hubert Llanes <6128470+hllanes@users.noreply.github.com>
2018-06-02 12:47:05 -0700

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -2,7 +2,7 @@ job('NodeJS Docker example') {
     scm {
         git('git://github.com/hllanes/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('hllanes@yahoo.com')
         }
     }
     triggers {
